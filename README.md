9442dea665886449828f8e2768472c2f9485e979
Rohan Raja <rohanraja9@gmail.com>
2016-03-03 16:58:22 +0530

    Added xyfinder for getting coordinates

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # patel-opensoft16
 
-To run the tests, execute:
+- To run the tests, execute:
+> py.test -s plot_test.py
 
-py.test -s plot_test.py
+- The Python script xyfinder.py opens a new window with the image and clicking on any point in the image will print the coordinates of the clicked location. Use this to get input and output hardcode values for writing tests.
+> python xyfinder.py
+
---
+++ xyfinder.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('img/tstplot.jpg')
+gray = cv2.imread('img/tstplot.jpg',0)
+
+ret,thresh = cv2.threshold(gray,127,255,1)
+
+contours,h = cv2.findContours(thresh,1,2)
+
+
+
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+
+    if len(approx)==4:
+        if cv2.contourArea(cnt) > 283060.0 :
+            print approx, cv2.contourArea(cnt)
+            # cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+
+def onmouse(event,x,y,flags,param):
+    if( event == cv2.EVENT_MOUSEMOVE ) :
+        return
+    print x, y
+
+cv2.imshow('img',img)
+cv2.setMouseCallback('img',onmouse)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
