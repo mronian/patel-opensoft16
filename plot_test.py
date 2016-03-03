69b9375104d5733e055118421b59e3f038013857
Rohan Raja <rohanraja9@gmail.com>
2016-03-03 16:41:25 +0530

    More tests

--- plot_test.py
+++ plot_test.py
@@ -10,11 +10,57 @@ class TestPlot(unittest.TestCase):
         self.plot = Plot(img)
 
     def test_finding_corners(self):
+        """
+        Setup: 
+            - Input Test Image (Already done)
+        
+        Assertion:
+            - Compare the area of contour with the actual area
+        
+        Things to Find:
+            - Area of Bounding Rectangle
+            
+        """
         self.plot.findCorners()
         print "\n\nCorners:\n\n", self.plot.corners
         assert cv2.contourArea(self.plot.corners) > 321000
 
+    def test_xtick_coordinates(self):
+        """
+        Setup: 
+            - Input Test Image (Already done)
+            - Corners
+        
+        Assertion:
+            - Check if the calculated 1st tick point is approximately correct
+        
+        Things to Find:
+            - Corner Points
+            - Actual Coordinates of the 1st tick point
+            
+        """
+
+    def test_xtick_text(self):
+        """
+        Setup: 
+            - Input Test Image (Already done)
+            - Corners
+            - 1st Tick Point Coordinates
+        
+        Assertion:
+            - Check the OCR on bounding rect and match it with actual text
+        
+        Things to Find:
+            - Corner Points
+            - Actual Coordinates of the 1st tick point
+            - Actual text content corresponding to 1st tick
+            
+        """
+        hjhj
     def test_finding_seriespoints(self):
+        """
+        
+        """
 
         self.plot.corners = [
 
