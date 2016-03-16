6766cc512062e65e46e30dbf6e7df6a9bc0b3e31
sinha135 <sinha135@gmail.com>
2016-03-16 07:52:24 +0530

    updated

--- getbounds.py
+++ getbounds.py
@@ -1,9 +1,5 @@
-"""
-Rohit
-"""
-import cv2
 
-pad = 3
+
 def getbounds(img,coordinate):
 
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -16,7 +12,7 @@ def getbounds(img,coordinate):
 
 
     r=hy+2
-    textthreshold=5
+    textthreshold=3
 
 
     threshold=2
@@ -46,7 +42,7 @@ def getbounds(img,coordinate):
             r2=r
             check=0
             if count >threshold :
-                xcoord.append([r1 - pad,r2 + pad])
+                xcoord.append([r1,r2])
             count =0
             if len(xcoord)==3 :
                 break
@@ -59,24 +55,23 @@ def getbounds(img,coordinate):
     '''    print "v",v,"count",count,"r",r
         print check '''
     
-    # for i in xcoord :
-    #     print i[0] , i[1]
-    #     cv2.line(img,(lx,i[0]),(hx,i[0]),(10,30,80),1)    
-    #     cv2.line(img,(lx,i[1]),(hx,i[1]),(10,30,80),1)
-    # showimg(img)
+    '''for i in xcoord :
+        print i[0] , i[1]
+        cv2.line(img,(lx,i[0]),(hx,i[0]),(10,30,80),1)    
+        cv2.line(img,(lx,i[1]),(hx,i[1]),(10,30,80),1)'''
 
     c=lx
     flag=1
     v=0
     ycoord=[]
     count=0
-    check
+    check=0
     textarea=0
     c1=0
     c2=0
     prev_gap=0
     gap=0
-    mingap=5
+    mingap=3
     textarea=0
     while flag==1:
         for r in range( ly,hy):
@@ -102,7 +97,7 @@ def getbounds(img,coordinate):
             
                 
             if count>threshold :
-                ycoord.append([c - pad ,c1 + pad])
+                ycoord.append([c,c1])
             if check==1 and count>2:
                     gap=0
             gap=gap+1
@@ -114,5 +109,69 @@ def getbounds(img,coordinate):
         c=c-1
         v=0
 
+    headthresh=6
+    v1=0;
+    v2=0
+    count1=0
+    count2=0
+    for h in range(0,100):
+
+        for c in range (lx,hx):        
+            if(ly-h>0):
+                
+                if img[ly-h,c]==0 :
+                    v1=v1+1
+            if(xcoord[1][1]+h<img.shape[0]):           
+                if img[xcoord[1][1]+h][c]==0:
+                    v2=v2+1
+        
+        if ly-h>0 :
+            
+            if v1>textthreshold:
+                count1 = count1+1
+                print "count1",count1
+            if count1>headthresh:
+                v1=0
+                y1=ly-h
+                r1=y1+headthresh
+                while 1:
+                    for c in range(lx,hx):
+                        if img[y1][c]==0:
+                            v1=v1+1
+                    
+                    if v1>textthreshold:
+                        y1=y1-1
+                    
+                    else:
+                        
+                        xcoord.append([y1,r1])
+                        
+                        break
+                    v1=0
+                break
+        
+        if(xcoord[1][1]+h<img.shape[0]):
+            if v2>textthreshold:
+                count2 = count2+1
+
+            if count2>headthresh:
+                v2=0
+                y2=xcoord[1][1]+h
+                r2=y2-headthresh
+                
+                while(1):                
+                    for c in range(lx,hx):
+                        if img[y2][c]==0:
+                            v2=v2+1
+                    
+                    if v2>textthreshold:
+                        y2=y2+1
+                    else:
+                        xcoord.append([r2,y2])
+                        break
+                    v2=0
+                break
+        v1=0
+        v2=0
            
     return (xcoord,ycoord)   
